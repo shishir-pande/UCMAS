7f2a6e6401a7cdb957a988e3f8140d3890a46add
Shishir Pande <32890115+shishir-pande@users.noreply.github.com>
2018-02-17 17:16:24 -0500

    Delete stripe_env.rb

--- config/stripe_env.rb
+++
@@ -1,7 +0,0 @@
-# This file contains the ENV vars necessary to run the app locally.  
-# Some of these values are sensitive, and some are developer specific.
-#
-# DO NOT CHECK THIS FILE INTO VERSION CONTROL
-
-ENV['STRIPE_TEST_PK'] =	'pk_test_zQwDo5IS46OTZ3yraHU2COyu'
-ENV['STRIPE_TEST_SK'] =	'sk_test_4vUPZwXqqtjQeoDHqdQ2ahhc'
